d4992ac8481c100800dcbaf5267275de762c5683
komninoschat <komninoschat@gmail.com>
2021-04-21 15:11:09 +0000

    fix: check ticket_expires_at on selectAccountByTicket

--- src/shared/queries.ts
+++ src/shared/queries.ts
@@ -103,7 +103,7 @@ export const selectAccountByEmail = gql`
 
 export const selectAccountByTicket = gql`
   query($ticket: uuid!) {
-    auth_accounts(where: { ticket: { _eq: $ticket } }) {
+    auth_accounts(where: { _and: [{ ticket: { _eq: $ticket } }, { ticket_expires_at: { _gt: $now } }] }) {
       ...accountFragment
     }
   }
